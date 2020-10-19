bfb4c0b14588ed990282df71f9ffaf83a97fb939
Christopher Henderson <chenderson5280@gmail.com>
2020-10-19 17:40:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
-def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer (data)
+  final_result = data.each_with_object ({}) do  |(key, value), final_array|
+  	   value.each  do |inner_key, name1|
+  	       name1.each  do |name2|
+  		if !final_array[name2]
+  		  final_array[name2] = {}
+  		end
+  		if !final_array[name2][key]
+  		  final_array[name2][key] = []
+  		end
+  	     final_array[name2][key].push(inner_key)
+  	end
+  	end
+  end
+  end
+
 
 end
